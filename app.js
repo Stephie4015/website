6c284c03176233d2e17474fa00efdcad4df7d9aa
Stephie4015 <160419585+Stephie4015@users.noreply.github.com>
2024-06-21 00:55:32 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+function testConfirmDialog() {
+var result = confirm("Do you want to continue?");
+if(result) 
+{ alert("OK Next lesson!");
+} 
+else
+{
+alert("Bye!");
+}
+}
+</script>
